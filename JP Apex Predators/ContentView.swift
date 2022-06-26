1c17b98585188a7d804ed87d592e2831f93f291e
Todd James <todd@itodd.co>
2022-06-26 08:40:53 -0400

    Initial Commit

---
+++ JP Apex Predators/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JP Apex Predators
+//
+//  Created by Todd James on 6/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Todd James on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

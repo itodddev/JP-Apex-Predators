963e902dfd2369388cbb9458be1f800f4221e472
Todd James <todd@itodd.co>
2022-06-27 16:35:07 -0400

    Finished App

--- JP Apex Predators/ContentView.swift
+++ JP Apex Predators/ContentView.swift
@@ -10,17 +10,19 @@ import SwiftUI
 struct ContentView: View {
   let apController = PredatorController() // create instance of a Predator Controller and runs init to decode JSON data
   @State var sortAlphabetical = false
+  @State var currentFilter = "All"
 
   var body: some View {
+    apController.filterBy(type: currentFilter) // Filter by types first
 
-    // Need to add return NavigationView, or the below if statement thinks the one line codes are trying to implicitly return a View
+    // Need to add return to NavigationView, or the below if statement thinks the one liners are trying to implicitly return a View
     if sortAlphabetical {
       apController.sortByAlphabetical()
     } else {
       apController.sortByMovieAppearance()
     }
 
-    // returns signle view
+    // returns single view
     return NavigationView {
       List {
         ForEach(apController.apexPredators) { predator in
@@ -33,14 +35,32 @@ struct ContentView: View {
       .toolbar {
         ToolbarItem(placement: .navigationBarLeading) {
           Button {
-            sortAlphabetical.toggle() // toggles state change, View refreshed and re-sorted
+            withAnimation {  // creates a shuffle effect
+              sortAlphabetical.toggle() // toggles state change, View refreshed and re-sorted
+            }
           } label: {
             if sortAlphabetical {
               Image(systemName: "film")
             } else {
               Image(systemName: "textformat")
-              
             }
+          }
+        }
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Menu {
+            Picker("Filter", selection: $currentFilter.animation()) { // $currentFilter bound to Picker, will update State and cause re-render also adds animation picker menu bounces out
+              ForEach(apController.typeFilters, id:\.self) { type in // make string array identifiable
+                HStack {
+                  Text(type)
+
+                  Spacer()
+
+                  Image(systemName: apController.typeIcon(for: type))  // icon names in switch statement in controller
+                }
+              }
+            }
+          } label: {
+            Image(systemName: "slider.horizontal.3")
           }
         }
       }
--- JP Apex Predators/PredatorController.swift
+++ JP Apex Predators/PredatorController.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 class PredatorController {
+  private var allApexPredators: [ApexPredator] = []
   var apexPredators: [ApexPredator] = []
+  let typeFilters = ["All", "Land", "Air", "Sea"]
 
   init() {
     decodeApexPredatorData()
@@ -20,7 +22,8 @@ class PredatorController {
         let data = try Data(contentsOf: url)
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase  // allows decoding from json snake_case to camel case
-        apexPredators = try decoder.decode([ApexPredator].self, from: data)
+        allApexPredators = try decoder.decode([ApexPredator].self, from: data)  // use the all array
+        apexPredators = allApexPredators // apexPredators will be used for filtering below
       } catch {
         print("Error decoding JSON data: \(error)")
       }
@@ -28,7 +31,7 @@ class PredatorController {
   }
 
   func sortByAlphabetical() {
-    apexPredators.sort(by: {
+    apexPredators.sort(by: {  // sort: inplace - sorts actual array order
       $0.name < $1.name
     })
   }
@@ -37,5 +40,22 @@ class PredatorController {
     apexPredators.sort(by: {
       $0.id < $1.id
     })
+  }
+
+  func typeIcon(for type: String) -> String {
+    switch type {
+    case "All": return "square.stack.3d.up.fill"
+    case "Land": return "leaf.fill"
+    case "Air": return "wind"
+    case "Sea": return "drop.fill"
+    default: return "questionmark"
+    }
+  }
+
+  func filterBy(type: String) {
+    switch type {
+    case "Land", "Air", "Sea": apexPredators = allApexPredators.filter { $0.type == type.lowercased()}  // filter: true == keep, false == remove, not implace creates a temp array and assigns it back to orig array
+    default: apexPredators = allApexPredators // "All" would be included in default clause
+    }
   }
 }
